14862c85b3e756bff9ab542a2a61cdbbca931fa5
Lakyn <lakynmphotography@hotmail.com>
2021-09-16 19:57:08 -0400

    final changes

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -36,53 +36,7 @@ const commentdata = [
     user_id: 6,
     post_id: 7
   },
-  {
-    comment_text: 'Sed vel enim sit amet nunc viverra dapibus.',
-    user_id: 2,
-    post_id: 4
-  },
-  {
-    comment_text: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.',
-    user_id: 3,
-    post_id: 12
-  },
-  {
-    comment_text: 'Morbi a ipsum.',
-    user_id: 4,
-    post_id: 20
-  },
-  {
-    comment_text: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
-    user_id: 2,
-    post_id: 14
-  },
-  {
-    comment_text: 'Donec ut mauris eget massa tempor convallis.',
-    user_id: 1,
-    post_id: 4
-  },
-  {
-    comment_text:
-      'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
-    user_id: 6,
-    post_id: 9
-  },
-  {
-    comment_text:
-      'Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.',
-    user_id: 5,
-    post_id: 14
-  },
-  {
-    comment_text: 'Quisque porta volutpat erat.',
-    user_id: 6,
-    post_id: 2
-  },
-  {
-    comment_text: 'Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.',
-    user_id: 3,
-    post_id: 2
-  },
+ 
 
 ];
 
